bbd416d1fa59641853c8b4880026ec79631ea1b0
jyotikumari01310 <jyotikumari01310@gmail.com>
2025-07-25 19:39:59 +0530

    Add files via upload

---
+++ library management.py
@@ -0,0 +1,31 @@
+
+# PROJECT ON LIBRARY MANAGEMENT SYSTEM
+import database
+import menulib
+import book
+import issue
+
+database.DatabaseCreate()
+database.TablesCreate()
+
+
+while True:
+    book.clrscreen()
+    print("\t\t\t Library Management\n")
+    print("=====================================================================")
+    print("1. Book Management")
+    print("2. Members Management")
+    print("3. Issue/Return Book")
+    print("4. Exit")
+    choice = int(input("Enter Choice between 1 to 4 -------> : "))
+    if choice == 1:
+        menulib.Menubook()
+    elif choice == 2:
+        menulib.MenuMember()
+    elif choice == 3:
+        menulib.MenuIssueReturn()
+    elif choice == 4:
+        break
+    else:
+        print("Wrong Choice.....Enter Your Choice again")
+        x = input("Press any key to continue")
